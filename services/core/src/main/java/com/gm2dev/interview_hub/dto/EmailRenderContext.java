bba3cf2781726da9ed9ab33f2bb51117d8215c1e
lcarera <luciano.carera@gm2dev.com>
2026-04-01 11:04:31 -0300

    chore: delete Cloud Tasks email infrastructure from core

--- services/core/src/main/java/com/gm2dev/interview_hub/dto/EmailRenderContext.java
+++
@@ -1,3 +0,0 @@
-package com.gm2dev.interview_hub.dto;
-
-public record EmailRenderContext(String frontendUrl) {}
--- services/core/src/main/java/com/gm2dev/interview_hub/service/EmailSender.java
+++
@@ -1,7 +0,0 @@
-package com.gm2dev.interview_hub.service;
-
-import com.gm2dev.interview_hub.dto.EmailTaskPayload;
-
-public interface EmailSender {
-    void send(EmailTaskPayload payload);
-}
